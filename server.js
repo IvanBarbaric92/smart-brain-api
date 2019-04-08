8a634d4ae4aa6f445483965518aa86acc0ca2979
Ivan.Barbaric <ivan.barbaric992@gmail.com>
2019-04-08 22:54:32 +0200

    update

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-Parser');
+const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
